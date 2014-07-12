e64c75d128e831e1cd8a74c4443db62c37ee5e68
Dennis Leon <dennis.leon39@gmail.com>
2014-07-13 00:56:46 +1000

    Fix file source uri

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@
 #class bash_config_scripts($username) {
 class common-scripts($username="") {
 file { "/Users/$username/config":
-    source  => "puppet://config",
+    source  => "puppet:///modules/common-scripts/config",
     recurse => true,
     }
 }
