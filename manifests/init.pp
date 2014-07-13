743f560c18b97928c8a63103b351409fba391228
Dennis Leon <dennis.leon39@gmail.com>
2014-07-13 18:53:41 +1000

    Add oh-my-zsh custom plugins for p and idea completion scripts

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,4 +11,8 @@ file { "/Users/$username/.gitconfig":
 file { "/Users/$username/Library/Preferences/net.limechat.LimeChat.plist":
     source  => "puppet:///modules/common-scripts/limechat/net.limechat.LimeChat.plist",
     }
+file { "/Users/$username/.oh-my-zsh/custom/plugins":
+    source  => "puppet:///modules/common-scripts/ohmyzsh/custom/plugins",
+    recurse => true,
+    }
 }
