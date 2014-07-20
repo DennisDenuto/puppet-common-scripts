794ba289467b3aca3525fec04d999a876a3a5bd5
Dennis Leon <dennis.leon39@gmail.com>
2014-07-20 13:43:25 +1000

    Add vim pathogen setup

---
+++ manifests/vim.pp
@@ -0,0 +1,36 @@
+nstalls vim and vim-pathogen
+
+# Examples
+#
+#   include vim
+#   vim::loader
+#   vim::bundle { 'syntastic':
+#     source => 'scrooloose/syntastic',
+#   }
+#
+class vim {
+  $home = "/Users/${::boxen_user}"
+  $vimrc = "${home}/.vimrc"
+  $vimdir = "${home}/.vim"
+
+  file { [$vimdir,
+    "${vimdir}/autoload",
+    "${vimdir}/bundle"]:
+    ensure  => directory,
+    recurse => true,
+  }
+
+  repository { "${vimdir}/vim-pathogen":
+    source => 'tpope/vim-pathogen'
+  }
+
+  file { "${vimdir}/autoload/pathogen.vim":
+    target  => "${vimdir}/vim-pathogen/autoload/pathogen.vim",
+    require => [
+      File[$vimdir],
+      File["${vimdir}/autoload"],
+      File["${vimdir}/bundle"],
+      Repository["${vimdir}/vim-pathogen"]
+    ]
+  }
+}
