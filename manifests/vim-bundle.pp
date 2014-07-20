420c95babbcc69df4c64d3d077827b44be73664b
Dennis Leon <dennis.leon39@gmail.com>
2014-07-20 15:11:19 +1000

    Revert "change from array to arguments"
    This reverts commit bdcb91deb6817e3cbaa64a40160624a82132bb1b.

--- manifests/vim-bundle.pp
+++ manifests/vim-bundle.pp
@@ -1,6 +1,24 @@
 # Will fetch and save gihub repositories in your bundle directory
-define common-scripts::vim-bundle($name, $repo) {
-  repository { "/Users/${::boxen_user}/.vim/bundle/${repo}":
-    source  => "${name}/${repo}",
+define common-scripts::vim-bundle() {
+
+  notify {"Running with bundle!!":
+      withpath => true,
+  }
+
+  notify {"name = $name":
+      withpath => true,
+  }
+
+  $bundle = split($name, '/')
+  notify {"bundle = $bundle":
+      withpath => true,
+  }
+
+  $bundleName = $bundle[1]
+  notify {"bundleName = $bundleName":
+      withpath => true,
+  }
+  repository { "/Users/${::boxen_user}/.vim/bundle/${bundleName}":
+    source  => $name,
   }
 }
