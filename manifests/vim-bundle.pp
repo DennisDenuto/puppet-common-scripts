9148cb96a6c1a504d28888dae6d142b7369ee565
Dennis Leon <dennis.leon39@gmail.com>
2014-07-20 15:12:45 +1000

    help with interoplating a variable with a dash in it

--- manifests/vim-bundle.pp
+++ manifests/vim-bundle.pp
@@ -1,24 +1,9 @@
 # Will fetch and save gihub repositories in your bundle directory
 define common-scripts::vim-bundle() {
 
-  notify {"Running with bundle!!":
-      withpath => true,
-  }
-
-  notify {"name = $name":
-      withpath => true,
-  }
-
-  $bundle = split($name, '/')
-  notify {"bundle = $bundle":
-      withpath => true,
-  }
-
-  $bundleName = $bundle[1]
-  notify {"bundleName = $bundleName":
-      withpath => true,
-  }
+  $bundle = split("${name}", '/')
+  $bundleName = "$bundle[1]"
   repository { "/Users/${::boxen_user}/.vim/bundle/${bundleName}":
-    source  => $name,
+    source  => "$name",
   }
 }
