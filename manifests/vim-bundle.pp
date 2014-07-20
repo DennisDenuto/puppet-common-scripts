a92f46a5354d162ee93498ea5fd512959f08c435
Dennis Leon <dennis.leon39@gmail.com>
2014-07-20 14:50:57 +1000

    split out vim bundle array

--- manifests/vim-bundle.pp
+++ manifests/vim-bundle.pp
@@ -1,7 +1,8 @@
 # Will fetch and save gihub repositories in your bundle directory
 define common-scripts::vim-bundle() {
   $bundle = split($name, '/')
-  repository { "/Users/${::boxen_user}/.vim/bundle/${bundle[1]}":
+  $bundleName = $bundle[1]
+  repository { "/Users/${::boxen_user}/.vim/bundle/${bundleName}":
     source  => $name,
   }
 }
