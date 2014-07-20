c1448e8e87ad6c51beeb71385969d90dde0e8bf0
Dennis Leon <dennis.leon39@gmail.com>
2014-07-20 15:48:40 +1000

    remove vim files. everything is in init class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,6 @@
 # This is a placeholder class.
 #class bash_config_scripts($username) {
 class common-scripts($username="") {
-include common-scripts::vim
 
 file { "/Users/$username/config":
     source  => "puppet:///modules/common-scripts/config",
@@ -17,4 +16,42 @@ file { "/Users/$username/.oh-my-zsh/custom/plugins":
     source  => "puppet:///modules/common-scripts/ohmyzsh/custom/plugins",
     recurse => true,
     }
+
+
+  $home = "/Users/${::boxen_user}"
+  $vimrc = "${home}/.vimrc"
+  $vimdir = "${home}/.vim"
+
+  file { [$vimdir,
+    "${vimdir}/autoload",
+    "${vimdir}/bundle"]:
+    ensure  => directory,
+    recurse => true,
+  }
+
+  repository { "${vimdir}/vim-pathogen":
+    source => 'tpope/vim-pathogen'
+  }
+
+  file { "${vimdir}/autoload/pathogen.vim":
+    target  => "${vimdir}/vim-pathogen/autoload/pathogen.vim",
+    require => [
+      File[$vimdir],
+      File["${vimdir}/autoload"],
+      File["${vimdir}/bundle"],
+      Repository["${vimdir}/vim-pathogen"]
+    ]
+  }
+
+  repository { "/Users/${::boxen_user}/.vim/bundle/vimcolorssolarized":
+    source  => "altercation/vimcolorssolarized",
+  }
+
+  # Install pathogen into .vimrc
+  file_line { 'load_pathogen':
+    ensure  => present,
+    line    => 'execute pathogen#infect()',
+    path    => $::vimrc,
+    require => File[$::vimrc]
+  }
 }
--- manifests/vimbundle.pp
+++
@@ -1,6 +0,0 @@
-# Will fetch and save gihub repositories in your bundle directory
-define common-scripts::vimbundle() {
-  repository { "/Users/${::boxen_user}/.vim/bundle/vimcolorssolarized":
-    source  => "altercation/vimcolorssolarized",
-  }
-}
