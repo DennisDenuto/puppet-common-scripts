32cc1950d506883c42b6570a905a6876b82db28f
Dennis Leon <dennis.leon39@gmail.com>
2014-07-20 14:57:16 +1000

    Add debug messages

--- manifests/vim-bundle.pp
+++ manifests/vim-bundle.pp
@@ -1,7 +1,23 @@
 # Will fetch and save gihub repositories in your bundle directory
 define common-scripts::vim-bundle() {
+
+  notify {"Running with bundle!!":
+      withpath => true,
+  }
+
+  notify {"name = $name":
+      withpath => true,
+  }
+
   $bundle = split($name, '/')
+  notify {"bundle = $bundle":
+      withpath => true,
+  }
+
   $bundleName = $bundle[1]
+  notify {"bundleName = $bundleName":
+      withpath => true,
+  }
   repository { "/Users/${::boxen_user}/.vim/bundle/${bundleName}":
     source  => $name,
   }
