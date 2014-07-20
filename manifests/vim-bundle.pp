87dc099ccde9557496be80130e1033aea71fe7dc
Dennis Leon <dennis.leon39@gmail.com>
2014-07-20 15:16:02 +1000

    no more array for bundle names

--- manifests/vim-bundle.pp
+++ manifests/vim-bundle.pp
@@ -1,9 +1,6 @@
 # Will fetch and save gihub repositories in your bundle directory
-define common-scripts::vim-bundle() {
-
-  $bundle = split("${name}", '/')
-  $bundleName = "$bundle[1]"
-  repository { "/Users/${::boxen_user}/.vim/bundle/${bundleName}":
-    source  => "$name",
+define common-scripts::vim-bundle($name, $repo) {
+  repository { "/Users/${::boxen_user}/.vim/bundle/${repo}":
+    source  => "${name}/${repo}",
   }
 }
