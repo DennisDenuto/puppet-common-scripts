6c6d9acf138ddae923a44055902dd9995f874b03
Dennis Leon <dennis.leon39@gmail.com>
2014-07-20 15:22:47 +1000

    remove dash from name

--- manifests/vimbundle.pp
+++ manifests/vimbundle.pp
@@ -1,6 +1,6 @@
 # Will fetch and save gihub repositories in your bundle directory
-define common-scripts::vim-bundle($name, $repo) {
+define common-scripts::vimbundle($repo_name, $repo) {
   repository { "/Users/${::boxen_user}/.vim/bundle/${repo}":
-    source  => "${name}/${repo}",
+    source  => "${repo_name}/${repo}",
   }
 }
