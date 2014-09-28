e4ba130af51493035203e983aa26098ca31e4698
Dennis Leon <dennis.leon39@gmail.com>
2014-09-28 21:54:16 +1000

    Added vim-scala

--- manifests/init.pp
+++ manifests/init.pp
@@ -54,6 +54,11 @@ file { "/Users/$username/.oh-my-zsh/custom/plugins":
   repository { "/Users/${::boxen_user}/.vim/bundle/vim-json":
     source  => "elzr/vim-json",
   }
+
+  repository { "/Users/${::boxen_user}/.vim/bundle/vim-scala":
+    source  => "derekwyatt/vim-scala",
+  }
+
   repository { "/Users/${::boxen_user}/.vim/bundle/vim-java-decompiler-using-jad":
     source  => "vim-scripts/JavaDecompiler.vim",
   }
