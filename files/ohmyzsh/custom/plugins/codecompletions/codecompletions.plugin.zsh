70167146f24b4c71ca1aceb5ef1c31dc586b5944
Dennis Leon <dennis.leon39@gmail.com>
2014-12-19 20:59:59 +1100

    updated intellij script to ouse 14

--- files/ohmyzsh/custom/plugins/codecompletions/codecompletions.plugin.zsh
+++ files/ohmyzsh/custom/plugins/codecompletions/codecompletions.plugin.zsh
@@ -1,4 +1,4 @@
-CODE_DIR=~/app/projects/
+CODE_DIR=/Users/dennisleon/code
 
 p()
 {
@@ -10,26 +10,28 @@ cd $project_found
 }
 _p() { _files -W $CODE_DIR -/; }
 compdef _p p
-  
-idea()
-{
+
+idea () {
         local project_looking_for project_found
         project_looking_for=$1
-        if [[ -e `pwd`/$project_looking_for ]]; then
-                open -a IntelliJ\ IDEA\ 13 -e `pwd`/$project_looking_for
+        idea_version=14
+        if [[ -e `pwd`/$project_looking_for ]]
+        then
+                open -a IntelliJ\ IDEA\ $idea_version -e `pwd`/$project_looking_for
         fi
-        if [[ -e `pwd`/pom.xml && $project_looking_for = "pom.xml" ]]; then
-                open -a IntelliJ\ IDEA\ 13 -e `pwd`/pom.xml
+        if [[ -e `pwd`/pom.xml && $project_looking_for = "pom.xml" ]]
+        then
+                open -a IntelliJ\ IDEA\ $idea_version -e `pwd`/pom.xml
         fi
-
         project_found=$(find $CODE_DIR -name "$project_looking_for" -type d -maxdepth 1)
-        if [ -e $project_found/pom.xml ]; then
-                open -a IntelliJ\ IDEA\ 13 -e $project_found/pom.xml
+        if [ -e $project_found/pom.xml ]
+        then
+                open -a IntelliJ\ IDEA\ $idea_version -e $project_found/pom.xml
         else
                 idea_proj_file="`basename $project_found`.ipr"
-                open -a IntelliJ\ IDEA\ 13 -e $project_found/$idea_proj_file
+                open -a IntelliJ\ IDEA\ $idea_version -e $project_found/$idea_proj_file
         fi
-
 }
+
 _idea() { _files -W $CODE_DIR -/; }
 compdef _idea idea
