74866814e793a3cc026793d8885f0b4dcb48fad6
Dennis Leon <dennis.leon39@gmail.com>
2015-07-03 14:20:24 -0700

    install basic go lib and gopath env

--- manifests/go.pp
+++ manifests/go.pp
@@ -1,8 +1,35 @@
 class commonscripts::go {
 
-  file_line { 'GOPATH':
+  file_line { 'GOROOT':
     path => '/etc/zshenv',
     line => "export GOROOT=/opt/boxen/homebrew/Cellar/go/1.4.2/libexec",
   }
 
+  file_line { 'GOPATH':
+    path => '/etc/zshenv',
+    line => "export GOPATH=/Users/${boxen_user}/.go",
+  }
+
+  $go_directory = [
+      "/Users/${boxen_user}/.go/",
+      "/Users/${boxen_user}/.go/src/",
+      "/Users/${boxen_user}/.go/src/github.com/",
+      "/Users/${boxen_user}/.go/src/github.com/user/",
+      ]
+
+  file { $go_directory :
+        ensure  => directory,
+  }
+
+  exec { "go get golang.org/x/tools/cmd/godoc":
+    environment => ["GOPATH=/Users/${boxen_user}/.go"],
+    logoutput => true,
+    require => File[$go_directory],
+  }
+
+  exec { "go get golang.org/x/tools/cmd/vet":
+    environment => ["GOPATH=/Users/${boxen_user}/.go"],
+    logoutput => true,
+    require => File[$go_directory],
+  }
 }
